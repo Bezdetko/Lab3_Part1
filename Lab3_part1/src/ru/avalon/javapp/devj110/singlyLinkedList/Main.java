41e5b8bb1104825a9fa6e571bbed47f807344061
Bezdetko <bezdetk0@mail.ru>
2021-11-07 19:07:52 +0300

    Пустой проект

--- Lab3_part1/src/ru/avalon/javapp/devj110/singlyLinkedList/Main.java
+++ Lab3_part1/src/ru/avalon/javapp/devj110/singlyLinkedList/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ru.avalon.javapp.devj110.singlyLinked.List;
+package ru.avalon.javapp.devj110.singlyLinkedList;
 
 /**
  *
